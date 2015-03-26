a84c6f5fe496156b5765d690ef3a400e278b08d2
carol-capek <carol.capek@gmail.com>
2015-03-26 16:24:07 +0100

    test passwort

--- app/src/main/java/at/fhooe/mc/android/tetris/MainActivity.java
+++ app/src/main/java/at/fhooe/mc/android/tetris/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Surf
 
         //some Changes!!!
         //hahahaha Sven Sven Sven was here
+        //blablablab passwort?
 
         setContentView(R.layout.activity_main);
 
