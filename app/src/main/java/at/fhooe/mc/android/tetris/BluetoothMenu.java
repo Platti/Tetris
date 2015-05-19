8a3adb6a131f5599c02a3236a0f8056b79272d68
Platti <plattner.michael@gmail.com>
2015-05-19 15:33:53 +0200

    bluetooth connection test 1

--- app/src/main/java/at/fhooe/mc/android/tetris/BluetoothMenu.java
+++ app/src/main/java/at/fhooe/mc/android/tetris/BluetoothMenu.java
@@ -3,29 +3,64 @@ package at.fhooe.mc.android.tetris;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 import at.fhooe.mc.android.tetris.R;
 
-public class BluetoothMenu extends Activity implements View.OnClickListener {
+public class BluetoothMenu extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
 
     private static final int REQUEST_ENABLE_BT = 1;
+    private static final String NAME = "Tetris Bluetooth";
+    private static final UUID MY_UUID = UUID.randomUUID();
+    private static final int MESSAGE_READ = 2;
+    private static final int MESSAGE_TOAST = 5;
+    private static final String MESSAGE_KEY_TOAST = "Message for Toast";
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     DeviceArrayAdapter mArrayAdapter;
+
+    AcceptThread mAcceptThread;
+    ConnectThread mConnectThread;
+    ConnectedThread mConnectedThread;
+
+    private final Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case MESSAGE_READ:{
+
+                }
+                break;
+                case MESSAGE_TOAST:{
+                    Bundle data = msg.getData();
+                    Toast.makeText(getBaseContext(), data.getString(MESSAGE_KEY_TOAST), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    };
 
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -49,7 +84,7 @@ public class BluetoothMenu extends Activity implements View.OnClickListener {
         mArrayAdapter = new DeviceArrayAdapter(this);
 
         if (mBluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG);
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
             this.finish();
         }
 
@@ -77,6 +112,15 @@ public class BluetoothMenu extends Activity implements View.OnClickListener {
 
         Button b = (Button) findViewById(R.id.button_refresh);
         b.setOnClickListener(this);
+
+        ListView deviceList = (ListView) findViewById(R.id.listViewDevices);
+        deviceList.setAdapter(mArrayAdapter);
+        deviceList.setOnItemClickListener(this);
+
+        if (mAcceptThread == null) {
+            mAcceptThread = new AcceptThread();
+            mAcceptThread.start();
+        }
     }
 
     @Override
@@ -88,13 +132,6 @@ public class BluetoothMenu extends Activity implements View.OnClickListener {
             discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
             startActivity(discoverableIntent);
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ListView deviceList = (ListView) findViewById(R.id.listViewDevices);
-        deviceList.setAdapter(mArrayAdapter);
     }
 
     @Override
@@ -110,6 +147,201 @@ public class BluetoothMenu extends Activity implements View.OnClickListener {
                 mBluetoothAdapter.cancelDiscovery();
                 mArrayAdapter.clear();
                 mBluetoothAdapter.startDiscovery();
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> _parent, View _view, int _pos, long _id) {
+        if (mConnectThread == null) {
+            MyBluetoothDevice device = (MyBluetoothDevice) _parent.getAdapter().getItem(_pos);
+            mConnectThread = new ConnectThread(mBluetoothAdapter.getRemoteDevice(device.getAddress()));
+        }
+    }
+
+    public synchronized void manageConnectedSocket(BluetoothSocket socket) {
+
+        // Cancel the thread that completed the connection
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        // Cancel the accept thread because we only want to connect to one device
+        if (mAcceptThread != null) {
+            mAcceptThread.cancel();
+            mAcceptThread = null;
+        }
+
+        mConnectedThread = new ConnectedThread(socket);
+        mConnectedThread.start();
+
+        Message msg = mHandler.obtainMessage(MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(MESSAGE_KEY_TOAST,"Verbunden mit  " + mBluetoothAdapter.getName());
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+    }
+
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) {
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection was accepted
+                if (socket != null) {
+                    // Do work to manage the connection (in a separate thread)
+                    manageConnectedSocket(socket);
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Will cancel the listening socket, and cause the thread to finish
+         */
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            manageConnectedSocket(mmSocket);
+        }
+
+        /**
+         * Will cancel an in-progress connection, and close the socket
+         */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
             }
         }
     }
